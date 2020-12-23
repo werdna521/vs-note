ffa06b3d3e558799d7f2682ab23c78b0a398ad0b
Andrew Cen <48923485+werdna521@users.noreply.github.com>
2020-12-23 21:43:08 +0700

    feat: show note on custom view container

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import NoteProvider from './providers/note-provider';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -9,6 +10,9 @@ export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "vs-note" is now active!');
+
+  const noteProvider = new NoteProvider('');
+  vscode.window.registerTreeDataProvider('notes', noteProvider);
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
